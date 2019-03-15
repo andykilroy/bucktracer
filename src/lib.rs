797c7e52b4d1570b4f3a492b3e21c714ec508242
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2019-03-15 21:07:24 +0000

    define direction struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 
 const EPSILON: f64 = 1e-5;
 
-type Direction = (f64, f64, f64, f64);
+pub struct Direction(f64, f64, f64, f64);
 
-pub fn is_point((x, y, z, w): Direction) -> bool {
+pub fn is_point(Direction(x, y, z, w): Direction) -> bool {
     w == 1.0
 }
 
@@ -12,14 +12,17 @@ pub fn is_vector(v: Direction) -> bool {
 }
 
 pub fn point(x: f64, y: f64, z: f64) -> Direction {
-    (x, y, z, 1.0)
+    Direction(x, y, z, 1.0)
 }
 
 pub fn vector(x: f64, y: f64, z: f64) -> Direction {
-    (x, y, z, 0.0)
+    Direction(x, y, z, 0.0)
 }
 
-pub fn equalish((a1, a2, a3, a4): Direction, (b1, b2, b3, b4): Direction) -> bool {
+pub fn equalish(
+    Direction(a1, a2, a3, a4): Direction, 
+    Direction(b1, b2, b3, b4): Direction) -> bool {
+
     if a4 != b4 {
         return false;
     }
