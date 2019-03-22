19356291792b6f4f2bc3c869be8598676e1291cd
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2019-03-22 00:05:11 +0000

    transpose matrices

--- src/lib.rs
+++ src/lib.rs
@@ -378,4 +378,11 @@ impl Matrix {
                self[2][i], 
                self[3][i])
     }
+
+    pub fn transpose(&self) -> Matrix {
+        matrix((self[0][0], self[1][0], self[2][0], self[3][0]),
+               (self[0][1], self[1][1], self[2][1], self[3][1]),
+               (self[0][2], self[1][2], self[2][2], self[3][2]),
+               (self[0][3], self[1][3], self[2][3], self[3][3]))
+    }
 }
