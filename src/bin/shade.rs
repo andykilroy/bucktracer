d03334165000bcc0a39d31b7464a2251ed092619
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2019-08-27 14:20:05 +0100

    shade command uses new camera

--- src/bin/shade.rs
+++ src/bin/shade.rs
@@ -1,23 +1,26 @@
 use bucktracer::*;
 use std::io::{Result, stdout};
+use std::f64::consts::FRAC_PI_2;
 
 fn main() -> Result<()> {
 
     let mut materl = Material::default();
     materl.set_colour(colour(1.0, 0.2, 1.0));
-
-    let mut cam = camera(canvas(300, 300),
-                     (0.0, 0.0, 0.0), (60.0, 60.0, 0.0),
-                     (0.0, 0.0, 1.0));
     let light = point_light(point(0.0, 60.0, -20.0), white());
-    let ray_target = point(30.0, 30.0, 40.0);
     let mut s = unit_sphere();
     s.set_material(&materl);
     s.set_transform(&(translation(30.0, 30.0, 20.0) * scaling(7.5, 7.5, 7.5)));
+    let world = World::with(vec![s], vec![light]);
 
-    raytrace(&mut cam, &light, &s, ray_target);
+    let mut cam = Camera::new(300, 300, FRAC_PI_2);
+    cam.set_transform(view_transform(
+        point(30.0, 30.0, 0.0),
+        point(30.0, 30.0, 20.0),
+        vector(0.0, 1.0, 0.0)));
+    let canv = cam.render(&world);
+
     let mut stdout = stdout();
-    encode_ppm(cam.canvas(), &mut stdout)
+    encode_ppm(&canv, &mut stdout)
 }
 
 fn raytrace(
