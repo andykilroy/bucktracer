8fb4191de99558c8b7d7a6eb3dcc36badd488b65
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2019-10-23 08:07:41 +0100

    place a cylinder in the water scene

--- src/bin/water.rs
+++ src/bin/water.rs
@@ -17,10 +17,14 @@ fn main() -> Result<(), ExitFailure> {
         .set_pattern(Pattern::solid(colour(1.0, 0.5, 0.0)))
         .set_ambient(1.0)
         .set_specular(0.0);
+    let silver = *Material::default()
+        .set_pattern(Pattern::solid(colour(0.8, 0.8, 0.8)))
+        .set_reflective(0.5);
 
     let mut floor = plane();
     floor.set_material(*Material::default()
         .set_pattern(Pattern::checkers(RGB::white(), RGB::black()))
+        .set_pattern_to_object_spc(scaling(2.0, 2.0, 2.0))
     );
     floor.set_object_to_world_spc(translation(0.0, -10.0, 0.0));
 
@@ -51,12 +55,18 @@ fn main() -> Result<(), ExitFailure> {
         translation(0.0, 300.0, 4000.0) * scaling(400.0, 400.0, 400.0)
     );
 
+    let mut cyl = cylinder(true, -1.0, 1.0);
+    cyl.set_material(silver);
+    cyl.set_object_to_world_spc(
+        translation(3.0, 0.0, 3.0)
+    );
+
     let light = point_light(point(-10.0, 10.0, -10.0), RGB::white());
     let world = World::with(
         vec![light],
-        vec![floor, above, below, water_surface, sun, sky],
+        vec![floor, above, below, cyl, water_surface, sun, sky],
     );
-    let mut cam = Camera::new(2560, 1600, FRAC_PI_2);
+    let mut cam = Camera::new(1280, 800, FRAC_PI_2);
     cam.set_view_transform(view_transform(
         point(0.0, 1.5, -5.0),
         point(0.0, 0.0, 0.0),
