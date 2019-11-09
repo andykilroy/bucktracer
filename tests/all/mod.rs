80f4579d5018e6d76783d0893285feeb2216e275
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2019-11-09 20:14:23 +0000

    move tests to module all
    This removes an ambiguity about how the tests are started up by cargo.
    Cargo presumes any *.rs file is its own crate, and fires the tests
    accordingly.  This means tests in the top level of the tests/ directory
    cannot share any common test functions, like almost_eq.
    
    I don't want almost_eq to be redefined in each crate, so it's declared
    in integration_tests.rs.  For tests to use this function
    definition, they have to be in the integration_tests crate.  Hence why
    this commit moves them into all, which is a module in the
    integration_tests crate.

---
+++ tests/all/mod.rs
@@ -0,0 +1,10 @@
+mod math;
+mod shape;
+mod camera;
+mod canvas;
+mod patterns;
+mod phong;
+mod ppm;
+mod rays;
+mod world;
+
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,7 +1,5 @@
-mod shape;
-mod math;
-mod camera;
-mod phong;
+mod all;
+
 
 fn almost_eq(x1: f64, x2: f64) -> bool {
     f64::abs(x1 - x2) < 1e-5
