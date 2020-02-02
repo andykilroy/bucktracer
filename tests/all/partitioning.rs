ab5645d79f283e8e6477949705aadaf8a7a1faed
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2020-02-02 21:01:27 +0000

    partitioning integration test

--- tests/all/partitioning.rs
+++ tests/all/partitioning.rs
@@ -83,12 +83,40 @@ fn no_objects___generates_empty_group() {
 
 #[allow(non_snake_case)]
 #[test]
-fn one_shape___generates_one_group___because_it_defines_outermost_bounding_box() {
+fn one_shape___generates_one_node___because_it_defines_outermost_bounding_box() {
     let tri = triangle(point(1.0, 0.0, 0.0), point(0.0, 1.0, 0.0), point(0.0, 0.0, 1.0));
     let groups = partition(vec![tri.clone()]);
 
     assert_eq!(obj_at(&groups, &[0].clone()).unwrap().children().len(), 1);
     assert_eq!(obj_at(&groups, &[0, 0].clone()), Some(tri));
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn a_shape_placed_in_each_eighth() {
+    let root = unit_sphere();
+    let s0 = unit_sphere().set_object_to_world_spc( translation(-0.25, -0.25, -0.25) * scaling(0.25, 0.25, 0.25)).clone();
+    let s1 = unit_sphere().set_object_to_world_spc( translation(-0.25, -0.25,  0.25) * scaling(0.25, 0.25, 0.25)).clone();
+    let s2 = unit_sphere().set_object_to_world_spc( translation(-0.25,  0.25, -0.25) * scaling(0.25, 0.25, 0.25)).clone();
+    let s3 = unit_sphere().set_object_to_world_spc( translation(-0.25,  0.25,  0.25) * scaling(0.25, 0.25, 0.25)).clone();
+    let s4 = unit_sphere().set_object_to_world_spc( translation( 0.25, -0.25, -0.25) * scaling(0.25, 0.25, 0.25)).clone();
+    let s5 = unit_sphere().set_object_to_world_spc( translation( 0.25, -0.25,  0.25) * scaling(0.25, 0.25, 0.25)).clone();
+    let s6 = unit_sphere().set_object_to_world_spc( translation( 0.25,  0.25, -0.25) * scaling(0.25, 0.25, 0.25)).clone();
+    let s7 = unit_sphere().set_object_to_world_spc( translation( 0.25,  0.25,  0.25) * scaling(0.25, 0.25, 0.25)).clone();
+
+    let groups = partition(vec![
+        s0.clone(), s1.clone(), s2.clone(), s3.clone(), s4.clone(),
+        s5.clone(), s6.clone(), s7.clone(), root.clone()
+    ]);
+    assert_eq!(obj_at(&groups, &[0, 0].clone()), Some(root));
+    assert_eq!(obj_at(&groups, &[1, 0, 0].clone()), Some(s0));
+    assert_eq!(obj_at(&groups, &[2, 0, 0].clone()), Some(s1));
+    assert_eq!(obj_at(&groups, &[3, 0, 0].clone()), Some(s2));
+    assert_eq!(obj_at(&groups, &[4, 0, 0].clone()), Some(s3));
+    assert_eq!(obj_at(&groups, &[5, 0, 0].clone()), Some(s4));
+    assert_eq!(obj_at(&groups, &[6, 0, 0].clone()), Some(s5));
+    assert_eq!(obj_at(&groups, &[7, 0, 0].clone()), Some(s6));
+    assert_eq!(obj_at(&groups, &[8, 0, 0].clone()), Some(s7));
 }
 
 // TODO test when the shapes are in one plane (one of the axes has exactly one value)
